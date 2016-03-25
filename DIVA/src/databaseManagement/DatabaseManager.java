541cbb010e0cbf5f4457d2d21aca777b2d86c583
samahri <saud.amahri@gmail.com>
2016-03-24 22:24:53 -0700

    Update DatabaseManager.java

--- DIVA/src/databaseManagement/DatabaseManager.java
+++ DIVA/src/databaseManagement/DatabaseManager.java
@@ -1,6 +1,11 @@
 package databaseManagement;
 
+import java.sql.Connection;
+import java.sql.DriveManager;
+import java.sql.SQLException;
+
 /*Robin */
+/*edit by Sammy*/
 /**
  * DatabaseManager deals with connection to database
  * @invariant dbname database name 
@@ -8,17 +13,27 @@ package databaseManagement;
  */
 public class DatabaseManager {
 	
-	private String dbname;
-	private String pw;
+	//Database URL
+        //THIS IS SET TO A TEST DATABASE: CHANGE TO 'diva_main' WHEN DEPLOYED!!
+        private static final String CONN_STRING = "jdbc:mysql://localhost/diva_test";
+        
+        // Database User Name and Password
+        private static final String USERNAME = "diva";
+        private static final String PASSWORD = "DiVA$E2016&";
+        
+        private Connectionn conn;
 	
-	/**
+	/** 
 	 * Constructs a DatabaseManager
-	 * @param db a db name
+	 * @param db a db name 
 	 * @param pass an encrypted password
 	 * @pre db and pass are valid
 	 * @post a DatabasManager object is created
+	 * Note1: The database username and pwd are static final variables and do not need to be passed into these methods
+	 * Note2: Consider making them protected
 	 */
-	public DatabaseManager(String db, String pass) {
+	protected DatabaseManager() {
+		conn = null;
 	}
 	
 	/**
@@ -26,7 +41,15 @@ public class DatabaseManager {
 	 * @pre !isConnect()
 	 * @post isConnected() 
 	 */
-	public void connect() {
+	protected void connect() {
+		if ()
+		try{
+			conn = DriveManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+		} catch(SQLException e){
+			System.err.println(e);
+		} finally{
+			disconnect();
+		}
 	}
 	
 	/**
@@ -34,7 +57,12 @@ public class DatabaseManager {
 	 * @pre isConnected()
 	 * @post !isConnect()
 	 */
-	public void disconnect() {
+	private void disconnect() {
+		try{
+			conn.close();
+		} catch(SQLException e){
+			System.err.println(e);
+		}
 	}
 	
 	/**
