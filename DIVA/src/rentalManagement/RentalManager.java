a13f1e6af519627c7d3da18b790dffe147c85d9d
Robin <ryuan01@gmail,com>
2016-03-24 17:20:54 -0700

    ben's work

--- DIVA/src/rentalManagement/RentalManager.java
+++ DIVA/src/rentalManagement/RentalManager.java
@@ -1,58 +1,72 @@
 package rentalManagement;
 
-import java.util.Date;
-
-import accountManagement.Account;
-import vehicleManagement.Equipment;
-import vehicleManagement.Vehicle;
-
-// assumes param objects are already constructed, such as Vehicle, list of Equipments, Location, and Date.
 public class RentalManager {
-
-	// should be contained i think, since every branch should have a unique manager
-	private static ReserveManager reservMan;
 	
-	public RentalManager()
-	{
-		reservMan = new ReserveManager();
-	}
+		/**
+		 * A Rental Manager that creates, and modifies Rentals.
+		 * @pre RentalManager is already created.
+		 */
+		public RentalManager()
+		{
 	
-	public void createReservation(Date date, Vehicle v, Equipment[] equips, Location location, Account acc)
-	{
-		reservMan.addReservation(date, v, equips, location, acc);
-	}
+		}
 	
-	public void cancelReservation(Reservation r)
-	{
-		reservMan.removeReservation(r);
-	}
-	
-	// does not deal with illegal params
-	public Reservation[] searchReservation(Object o)
-	{
-		return null;
-		//return reservMan.searchReservations(o);
-	}
-	
-	public void changeReservationDetail(Object o)
-	{
-		//reservMan.changeReservation(o);
-	}
-	
-	public void addReservationEquipment(Reservation r, Equipment e)
-	{
-		//reservMan.addReservationEquipment(r, e);
-	}
-	
-	public void removeReservationEquipment(Reservation r, Equipment e)
-	{
-		//reservMan.removeReservationEquipment(r, e);
-	}
-	
-	public void createRental() {
-	}
-	
-	public void createReturn() {
-	}
-	
+		/**
+		 * Begins the Rental.
+		 * @param r Reservation of a Rental to be started, calls Database to record rental.
+		 * @pre Contract is already signed.Reservation has valid fields.
+		 * @post Payment is made. A Receipt is created. Rental is recorded in database.
+		 */
+		public void startRental(Reservation r)
+		{
+			// order of execution:
+			//payForRental(r, TypeOfPayment);
+			//recordRental(r);
+			//requestReceipt(r);
+		}
+		
+		/**
+		 * Cancels a Rental pre-emptively.
+		 * @param r Reservation of a Rental to be cancelled, calls Database to record rental.
+		 * @pre Rental is already started and has not been returned yet, should only be used in emergency, use Return 
+		 * for normal Rental closure.
+		 * @post Rental cancel is recorded in database.
+		 */
+		public void cancelRental(Reservation r)
+		{
+			
+		}
+		
+		/**
+		 * Calls Accounting system to pay for the Rental.
+		 * @param r Reservation of Rental to be paid.
+		 * @param typeOfPayment Debit for debit card, Credit for credit card, Cash for cash, SRP for SuperRent points.
+		 * @pre Reservation has valid fields, correct typeOfPayment is entered.
+		 */
+		public void payForRental(Reservation r, String typeOfPayment)
+		{
+			
+		}
+		
+		/**
+		 * Calls Accounting system to create a receipt for Reservation.
+		 * @param r Reservation of Rental to create Receipt for.
+		 * @pre Reservation has valid fields, and payment has been made.
+		 * @post Receipt is created.
+		 */
+		public void requestRentalReceipt(Reservation r)
+		{
+			
+		}
+		
+		/**
+		 * Calls Database system to record the Rental details.
+		 * @param r Reservation of Rental to record details for.
+		 * @pre Reservation has valid fields, payment has been made.
+		 * @post Rental is recorded in database.
+		 */
+		public void recordRental(Reservation r)
+		{
+			
+		}
 }
--- DIVA/src/rentalManagement/ReserveManager.java
+++ DIVA/src/rentalManagement/ReserveManager.java
@@ -1,178 +1,139 @@
 package rentalManagement;
 
-import java.util.ArrayList;
-import java.util.Date;
-
-import accountManagement.Account;
-import vehicleManagement.Equipment;
-import vehicleManagement.Vehicle;
-
 public class ReserveManager {
 	
-	// makes Reservations more searchable, otherwise reservations would be stored in Accounts.
-	private ArrayList<Reservation> listOfReservations;
-	
+	/**
+	 * A Manager for adding, removing, and modifying Reservations and its attributes.
+	 */
 	public ReserveManager()
 	{
-		listOfReservations = new ArrayList<Reservation>();
 	}
 	
 	// assumes Vehicle has status state and changeStatus method. Potential requires locking
-	public void addReservation(Date date, Vehicle vehicle,Equipment[] equips, Location location, Account acc)
+	/**
+	 * Creates a Reservation with a date, vehicle, list of equipments, location, and account.
+	 * @param date Date of the Reservation.
+	 * @param vehicle Vehicle of the Reservation.
+	 * @param equips Equips of the Reservation.
+	 * @param startB The Branch Vehicle is Rented.
+	 * @param endB The Branch Vehicle is Returned.
+	 * @param acc Account of the Reservation.
+	 */
+	public void addReservation(Date date, Vehicle vehicle, Equipment[] equips, Branch startB, Branch endB, Account acc)
 	{
-		/*reserv = new Reservation(date, vehicle, equips, location, acc);
-		listOfReservations.add(reserv);
-		vehicle.changeStatus("Reserved");*/
 	}
 	
 	// assumes Vehicle has status state and changeStatus method. Potential requires locking
-	public void removeReservation(Reservation r)
+	/**
+	 * Removes a Reservation that belongs to the Customer account itself.
+	 * @param c A Customer account.
+	 * @param r The Reservation to be removed.
+	 * @pre Reservation r belongs to Customer c
+	 */
+	public void removeReservation(Customer c,Reservation r)
 	{
-		/*
-		for(int i = 0; i < listOfReservations.size(); i++)
-		{
-			if(listOfReservations.get(i).equals(r))
-			{
-				listOfReservations.get(i).getVehicle.changeStatus("Available");
-				listOfReservations.remove(i);
-			}
-		}
-		*/
+	}
+	
+	/**
+	 * Removes a Reservation that belongs to anyone, can only be called by an Employee.
+	 * @param e An Employee account.
+	 * @param r The Reservation to be removed.
+	 * @pre Method is called by an Employee.
+	 */
+	public void removeReservation(Employee e,Reservation r)
+	{
 	}
 	
 	// Assume Account has equals() method *Overloaded
-	public ArrayList<Reservation> searchReservations(Account acc)
+	/**
+	 * Searches for the list of Reservations belonging to a Customer Account.
+	 * @param c The Customer Account to be searched.
+	 * @return List of Reservations belonging to a Customer.
+	 */
+	public Reservation[] searchReservations(Customer c)
 	{
-		return listOfReservations;
-		/*
-		tempReservList = new ArrayList<Reservation>();
-		for(int i = 0; i < listOfReservations.size(); i++)
-		{
-			if(listOfReservations.get(i).getAccount().equals(acc))
-			{
-				tempReservList.add(listOfReservations.get(i));
-			}
-		}
-		return tempReservList;
-		*/
 	}
 	
 	// assumes Vehicle has overriden equals() method *Overloaded
-	public ArrayList<Reservation> searchReservations(Vehicle v)
+	/**
+	 * Searches Reservations that a Vehicle is assigned to.
+	 * @param v Vehicle to search for.
+	 * @return List of Reservations that a Vehicle is assigned to.
+	 */
+	public Reservation[] searchReservations(Vehilce v)
 	{
-		return listOfReservations;
-		/*
-		tempReservList = new ArrayList<Reservation>();
-		for(int i = 0; i < listOfReservations.size(); i++)
-		{
-			if(listOfReservations.get(i).getVehicle().equals(v))
-			{
-				tempReservList.add(listOfReservations.get(i));
-			}
-		}
-		return tempReservList;
-		*/
 	}
 	
-	// only accessible for manager? *Overloaded
-	public ArrayList<Reservation> searchReservations(Location l)
+	//*Overloaded
+	/**
+	 * Searches Reservations that a Branch has.
+	 * @param b Branch to search for.
+	 * @return List of Reservations that a Branch has.
+	 */
+	public Reservation[] searchReservations(Branch b)
 	{
-		return listOfReservations;
-		/*
-		tempReservList = new ArrayList<Reservation>();
-		for(int i = 0; i < listOfReservations.size(); i++)
-		{
-			if(listOfReservations.get(i).getLocation().equals(l))
-			{
-				tempReservList.add(listOfReservations.get(i));
-			}
-		}
-		return tempReservList;
-		*/
 	}
 		
 	//*Overloaded
+	/**
+	 * Searches Reservations that a Date is assigned to.
+	 * @param d Date to search for.
+	 * @return List of Reservations that a Date is assigned to.
+	 */
 	public ArrayList<Reservation> searchReservations(Date d)
 	{
-		return listOfReservations;
-		/*
-		tempReservList = new ArrayList<Reservation>();
-		for(int i = 0; i < listOfReservations.size(); i++)
-		{
-			if(listOfReservations.get(i).getDate().equals(d))
-			{
-				tempReservList.add(listOfReservations.get(i));
-			}
-		}
 		return tempReservList;
-		*/
 	}
 		
 	//*Overloaded
+	/**
+	 * Modifies the Date of a Reservation.
+	 * @param r The Reservation to be modified.
+	 * @param newDate The Date to be modified.
+	 * @pre Only Reservation Account owner or Employee calls this method.
+	 */
 	public void changeReservation(Reservation r, Date newDate)
 	{
-		/*
-		for(int i = 0; i < listOfReservations.size(); i++)
-		{
-			if(listOfReservations.get(i).equals(r))
-			{
-				listOfReservations.changeDate(newDate);
-			}
-		}
-		*/
 	}
 
 	// *Overloaded
+	/**
+	 * Modifies the Location of a Reservation.
+	 * @param r The Reservation to be modified.
+	 * @param newLocation The Location to be modified.
+	 * @pre Only Reservation Account owner or Employee calls this method.
+	 */
 	public void changeReservation(Reservation r, Location newLocation)
 	{
-		/*
-		for(int i = 0; i < listOfReservations.size(); i++)
-		{
-			if(listOfReservations.get(i).equals(r))
-			{
-				listOfReservations.changeLocation(newLocation);
-			}
-		}
-		*/
 	}
 	
+	/**
+	 * Modifies the Vehicle of a Reservation.
+	 * @param r The Reservation to be modified.
+	 * @param newVehicle The Vehicle to be modified.
+	 * @pre Only Reservation Account owner or Employee calls this method.
+	 * @post All Equipments are also removed.
+	 */
 	public void changeReservation(Reservation r, Vehicle newVehicle)
 	{
-		/*
-		for(int i = 0; i < listOfReservations.size(); i++)
-		{
-			if(listOfReservations.get(i).equals(r))
-			{
-				listOfReservations.changeVehicle(newVehicle);
-			}
-		}
-		*/
 	}
 	
+	/**
+	 * Adds an Equipment to the Reservation.
+	 * @param r Reservation to be modified.
+	 * @param newEquipment Equipment to be added.
+	 */
 	public void addReservationEquipment(Reservation r, Equipment newEquipment)
 	{
-		/*
-		for(int i = 0; i < listOfReservations.size(); i++)
-		{
-			if(listOfReservations.get(i).equals(r))
-			{
-				listOfReservations.addEquipment(newEquipment);
-			}
-		}
-		*/
 	}
 	
+	/**
+	 * Removes an Equipment to the Reservation.
+	 * @param r Reservation to be modified
+	 * @param removedEquipment Equipment to be removed.
+	 */
 	public void removeReservationEquipment(Reservation r, Equipment removedEquipment)
 	{
-		/*
-		for(int i = 0; i < listOfReservations.size(); i++)
-		{
-			if(listOfReservations.get(i).equals(r))
-			{
-				listOfReservations.removeEquipment(removedEquipment);
-			}
-		}
-		*/
 	}
 	
 	
