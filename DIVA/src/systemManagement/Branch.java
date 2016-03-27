cc83df26b8e7ded6bb362aec2b6d569d61d97a02
samahri <saud.amahri@gmail.com>
2016-03-26 21:18:05 -0700

    Update Branch.java

--- DIVA/src/systemManagement/Branch.java
+++ DIVA/src/systemManagement/Branch.java
@@ -1,13 +1,26 @@
+/**
+ * Note: 
+ * 	1- Please notice the "issues" which are commented throughout the file
+ * 	2- The javadoc can be updated when this class is approved
+ * 
+ * This class has NOT BEEN TESTED
+ * 
+ * Changes:
+ * 	1- the removeVehicle() method: what if the vehicle is not there? it should return a boolean determining the success of the operation; 
+ */
 package systemManagement;
 
-//import vehicleManagement.Vehicle;
+import vehicleManagement.Vehicle;
+import vehicleManagement.Equipment;
+
 import java.util.ArrayList;
 
 public class Branch {
 	
 	private int id;
 	private String address;
-	// ArrayList<Vehicle> fleet;
+	private ArrayList<Vehicle> fleet;
+	private ArrayLIst<Equipment> inventory;
 	
 	public Branch(String address){
 		this.address = address;
@@ -16,6 +29,14 @@ public class Branch {
 	public Branch(Branch br){
 		br.getBranchID() = id
 		br.getAddress() = address;
+		
+		for (Vehicle v: br.getFleet()){
+			this.fleet.add(v);
+		}
+		
+		for (Equipment eq: br.getInventory()){
+			this.inventory.add(eq);
+		}
 	}
 	
 	// Getters and Setters
@@ -27,6 +48,14 @@ public class Branch {
 		return streetName;
 	}
 	
+	public ArrayList<Vehicle> getFleet(){
+		return fleet;
+	}
+	
+	public ArrayList<Equipment> getInventory(){
+		return inventory;
+	}
+	
 	
 	// public interface
 	/**
@@ -35,52 +64,51 @@ public class Branch {
 	 */
 	public void changeBranchAddress(String newBranchAddress)
 	{
-		
+		this.address = newBranchAddress;
 	}
 	
 	/**
 	 * Add a Vehicle ID to list of Vehicle IDs.
 	 * @param newVehicleID Vehicle ID to add to the list.
+	 * 
 	 */
-	public void addBranchVehicleID(String newVehicleID)
+	public void addVehicle(Vehicle newVehicle)
 	{
-		
+		this.fleet.add(newVehicle);
+		// ISSUE_1:The database needs to be updated of the addition process
 	}
 	
 	/**
 	 * Remove a Vehicle ID to list of Vehicle IDs.
 	 * @param newVehicleID Vehicle ID to be removed from the list.
+	 * @return success value, the vehicle might not be there
 	 */
-	public void removeBranchVehicleID(String removedVehicleID)
+	public boolean removeVehicle(Vehicle removeVeh)
 	{
+		for(Vehicle v: fleet){
+			if(v.getID() == removeVeh){
+				return fleet.remove(v);
+			}
+		}
+		//ISSUE_2: the database needs to be updated of the removal process
 		
+		return false;
 	}
 	
-	/**
-	 * Returns the Branch ID.
-	 * @return Branch ID to be returned.
-	 */
-	public String getBranchID()
-	{
-		return "";
+	public void addEquipment(Equipment newEquipment){
+		inventory.add(newEquipment);
+		// ISSUE_3:The database needs to be updated of the addition process for the equipment as well
 	}
 	
-	/**
-	 * Returns the Branch Address.
-	 * @return Branch address to be returned.
-	 */
-	public String getBranchAddress()
+	public boolean removeEquipment(Equipment removeEq)
 	{
-		return "";
+		for(Equipment eq: inventory){
+			if(eq.getID() == removeEq){
+				return inventory.remove(v);
+			}
+		}
+		//ISSUE_4: same database update notification
+		
+		return false;
 	}
-	
-	/**
-	 * Returns the list of Vehicle IDs.
-	 * @return List of Vehicle IDs.s
-	 */
-	public String[] getBranchVehicles()
-	{
-		return null;
-	}
-}
 }
