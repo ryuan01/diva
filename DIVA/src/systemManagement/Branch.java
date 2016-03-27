a765ee5dce3488ebcb3cb2341d4d2e082e24c800
samahri <saud.amahri@gmail.com>
2016-03-26 20:45:35 -0700

    Update Branch.java

--- DIVA/src/systemManagement/Branch.java
+++ DIVA/src/systemManagement/Branch.java
@@ -1,5 +1,40 @@
 package systemManagement;
 
+//import vehicleManagement.Vehicle;
+import java.util.ArrayList;
+
 public class Branch {
 	
+	private int id;
+	private String streetName;
+	private String city;
+	private String province;
+	private String zipcode;
+	// ArrayList<Vehicle> fleet;
+	
+	public Branch(String streetName, String city, String province, String zipcode){
+		this.streetName = streetName;
+		this.city = city;
+		this.province = province;
+		this.zipcode = zipcode;
+	}
+	
+	// Getters and Setters
+	public String getStreetName(){
+		return streetName;
+	}
+	
+	public String getCity(){
+		return city;
+	}
+	
+	public String getProvince(){
+		return province;
+	}
+	
+	public String getZipCode(){
+		return zipcode;
+	}
+	
+	// public interface
 }
