6886eaca8a5f195f2ebfc06423095f2bf45ab42a
samahri <saud.amahri@gmail.com>
2016-03-26 20:51:24 -0700

    Update Branch.java

--- DIVA/src/systemManagement/Branch.java
+++ DIVA/src/systemManagement/Branch.java
@@ -6,35 +6,81 @@ import java.util.ArrayList;
 public class Branch {
 	
 	private int id;
-	private String streetName;
-	private String city;
-	private String province;
-	private String zipcode;
+	private String address;
 	// ArrayList<Vehicle> fleet;
 	
-	public Branch(String streetName, String city, String province, String zipcode){
-		this.streetName = streetName;
-		this.city = city;
-		this.province = province;
-		this.zipcode = zipcode;
+	public Branch(String address){
+		this.address = address;
+	}
+	
+	public Branch(Branch br){
+		br.getBranchID() = id
+		br.getAddress() = address;
 	}
 	
 	// Getters and Setters
-	public String getStreetName(){
+	public int getBranchID(){
+		return id;
+	}
+	
+	public String getAddress(){
 		return streetName;
 	}
 	
-	public String getCity(){
-		return city;
-	}
-	
-	public String getProvince(){
-		return province;
-	}
-	
-	public String getZipCode(){
-		return zipcode;
-	}
 	
 	// public interface
+	/**
+	 * Modifies Branch Address.
+	 * @param newBranchAddress New Branch Address.
+	 */
+	public void changeBranchAddress(String newBranchAddress)
+	{
+		
+	}
+	
+	/**
+	 * Add a Vehicle ID to list of Vehicle IDs.
+	 * @param newVehicleID Vehicle ID to add to the list.
+	 */
+	public void addBranchVehicleID(String newVehicleID)
+	{
+		
+	}
+	
+	/**
+	 * Remove a Vehicle ID to list of Vehicle IDs.
+	 * @param newVehicleID Vehicle ID to be removed from the list.
+	 */
+	public void removeBranchVehicleID(String removedVehicleID)
+	{
+		
+	}
+	
+	/**
+	 * Returns the Branch ID.
+	 * @return Branch ID to be returned.
+	 */
+	public String getBranchID()
+	{
+		return "";
+	}
+	
+	/**
+	 * Returns the Branch Address.
+	 * @return Branch address to be returned.
+	 */
+	public String getBranchAddress()
+	{
+		return "";
+	}
+	
+	/**
+	 * Returns the list of Vehicle IDs.
+	 * @return List of Vehicle IDs.s
+	 */
+	public String[] getBranchVehicles()
+	{
+		return null;
+	}
+}
 }
