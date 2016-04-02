c931ae0f3b4429a8af202acfd01162d87e5f682e
Robin <ryuan01@gmail,com>
2016-04-02 15:19:40 -0700

    new sql

---
+++ DIVA/database/add.sql
@@ -0,0 +1,158 @@
+-- Populate branch
+-- this one is head-quarter
+INSERT INTO branch VALUES
+(1,100,'Manitoba Street','Vancouver','BC','V5Y2Z6');
+-- these ones are branches for renting and saling
+INSERT INTO branch VALUES
+(2,3211,'Grant McConachie Way','Richmond','BC','V7B 0A4');
+INSERT INTO branch VALUES
+(3,757,'Hornby Street','Vancouver','BC','V6Z1S2');
+
+-- Populate users
+-- three customers
+INSERT INTO users VALUES
+(1,'John','Doe','7780000000','joedoe@example.com','jdoe01','admin');
+INSERT INTO users VALUES
+(2,'Marry','Jane','7780000001','marryjane@example.com','mjane01','admin');
+INSERT INTO users VALUES
+(3,'James','Kurt','7780000003','jameskurt@example.com',null,null);
+-- five employees 
+INSERT INTO users VALUES
+(4,'Steward','Freeman','7780000004','stewardfreeman@example.com','sfreeman01','admin');
+INSERT INTO users VALUES
+(5,'Luke','Phillips','7780000005','lukephillips@example.com','lphillips01','admin');
+INSERT INTO users VALUES
+(6,'Noah','Johnson','7780000006','noahjohnson@example.com','njohnson01','admin');
+INSERT INTO users VALUES
+(7,'Omar','Barnes','7780000007','omarbarnes@example.com','obarnes01','admin');
+INSERT INTO users VALUES
+(8,'Elizabeth','Hunter','7780000008','ehunter@example.com','ehunter01','admin');
+
+-- define customers with account
+INSERT INTO customer VALUES
+(1, 'good', null,null);
+INSERT INTO customer VALUES
+(2, 'good', null, null);
+
+-- define super customer
+INSERT INTO super_customer (cus_id) VALUES
+(1);
+
+-- define employees
+INSERT INTO employee VALUES
+(4,2,'Clerk'); 
+INSERT INTO employee VALUES
+(5,2,'Manager'); 
+INSERT INTO employee VALUES
+(6,3,'Clerk'); 
+INSERT INTO employee VALUES
+(7,3,'Manager'); 
+INSERT INTO employee VALUES
+(8,1,'SystemAdmin'); 
+
+-- give each account some balance 0.0
+INSERT INTO account_balance (account_id) VALUES
+(1);
+INSERT INTO account_balance (account_id) VALUES
+(2);
+INSERT INTO account_balance (account_id) VALUES
+(3);
+INSERT INTO account_balance (account_id) VALUES
+(4);
+INSERT INTO account_balance (account_id) VALUES
+(5);
+INSERT INTO account_balance (account_id) VALUES
+(6);
+INSERT INTO account_balance (account_id) VALUES
+(7);
+INSERT INTO account_balance (account_id) VALUES
+(8);
+
+-- add a few vehicles, just 3 cars and 2 trucks for now
+INSERT INTO vehicle VALUES
+(1,'Chevrolet','2010-01-01','Spark','Green','for rent',null);
+INSERT INTO vehicle VALUES
+(2,'Chevrolet','2010-01-01','Sonic','White','for rent',null);
+INSERT INTO vehicle VALUES
+(3,'Chrysler','2010-01-01','200','Dark Blue','for rent',null);
+INSERT INTO vehicle VALUES
+(4,'U-Haul','2010-01-01',null,'White','for rent',null);
+INSERT INTO vehicle VALUES
+(5,'U-Haul','2010-01-01',null,'White','for rent',null);
+
+-- add car_price
+INSERT INTO car_price VALUES
+('economy', 3.00, 15.00, 80.00, 300.00, 0.25);
+INSERT INTO car_price VALUES
+('compact', 3.10, 15.99, 90.00, 520.00, 0.30);
+INSERT INTO car_price VALUES
+('midsized', 3.50, 18.00, 100.00, 680.00, 0.35);
+INSERT INTO car_price VALUES
+('standard', 3.60, 20.00, 140.00, 950.00, 0.40);
+INSERT INTO car_price VALUES
+('fullsized', 3.99, 25.00, 175.00, 1125.00,0.50);
+INSERT INTO car_price VALUES
+('premium', 4.25, 30.00, 210.00, 1300.00, 0.55);
+INSERT INTO car_price VALUES
+('luxury', 10.00, 80.00, 500.00, 1800.00, 0.88);
+INSERT INTO car_price VALUES
+('SUV', 4.25, 30.00, 210.00, 1300.00, 0.55);
+INSERT INTO car_price VALUES
+('van', 4.25, 30.00, 210.00, 1300.00, 0.55);
+
+-- add cars
+INSERT INTO car VALUES
+(1, 'economy', 2, '3/5', 1,1,5);
+INSERT INTO car VALUES
+(2, 'compact', 1, '3/5', 1,1,5);
+INSERT INTO car VALUES
+(3, 'midsized', 2, '4', 1,1,5);
+
+-- add truck_price
+INSERT INTO truck_price VALUES
+('24-foot', 8.00, 45.00, 300.00, 1100.00, 0.66);
+INSERT INTO truck_price VALUES
+('15-foot', 7.00, 40.00, 270.00, 1000.00, 0.60);
+INSERT INTO truck_price VALUES
+('12-foot', 6.50, 35.00, 250.00, 900.00, 0.55);
+INSERT INTO truck_price VALUES
+('box-truck', 6.00, 30.00, 220.00, 800.00, 0.50);
+
+-- add trucks
+INSERT INTO truck VALUES
+(4,'15-foot',7.08,8,15,1150);
+INSERT INTO truck VALUES
+(5,'24-foot',8.5,8,24,3200);
+
+-- add branch_vehicle, put them all in branch 2
+INSERT INTO branch_vehicle VALUES
+(1,2);
+INSERT INTO branch_vehicle VALUES
+(2,2);
+INSERT INTO branch_vehicle VALUES
+(3,2);
+INSERT INTO branch_vehicle VALUES
+(4,2);
+INSERT INTO branch_vehicle VALUES
+(5,2);
+
+-- add equipment_price
+INSERT INTO equipment_price VALUES
+('ski rack', 50.00, 10.00, 2.00);
+INSERT INTO equipment_price VALUES
+('child safety seat', 50.00, 10.00, 2.00);
+INSERT INTO equipment_price VALUES
+('lift gate', 60.00, 12.00, 3.00);
+INSERT INTO equipment_price VALUES
+('car-towing eq', 100.00, 18.00, 5.00);
+
+-- add quipment
+INSERT INTO equipment VALUES
+(1,2,'ski rack');
+INSERT INTO equipment VALUES
+(2,2,'child safety seat');
+
+
+-- to be done: insurance_car_price,
+-- insurance_truck_price, payment_log, rental, rental_equipment,
+-- report, vehicle_sale_price
---
+++ DIVA/database/database_ddl3.sql
@@ -0,0 +1,246 @@
+USE test;
+-- If the database already exists, execute the following code:
+-- create the User table 
+CREATE TABLE users (
+	id_number SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	first_name VARCHAR(20) NOT NULL,
+	last_name VARCHAR(20) NOT NULL,
+	phone BIGINT(13) NOT NULL,
+	email VARCHAR(20),
+	account_uName VARCHAR(20),
+	account_password VARCHAR(20)
+);
+
+-- create a account balance table 
+CREATE TABLE account_balance(
+	account_id SMALLINT UNSIGNED PRIMARY KEY,
+    balance DECIMAL(6,2) DEFAULT 0,
+    FOREIGN KEY (account_id) REFERENCES users(id_number)
+);
+
+-- create a payment log table
+CREATE TABLE payment_log(
+	customer_id SMALLINT UNSIGNED PRIMARY KEY,
+	amount DECIMAL(10,2) NOT NULL,
+    p_date DATE NOT NULL,
+	reason VARCHAR(200),
+    FOREIGN KEY (customer_id) REFERENCES users(id_number)
+);
+
+-- create a branch table
+CREATE TABLE branch(
+	br_num TINYINT(2) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	street_num SMALLINT UNSIGNED NOT NULL,
+	street_name VARCHAR(10) NOT NULL,
+	city VARCHAR(10) NOT NULL,
+	province SET('AB', 'BC', 'MB', 'NB', 'NL', 'NS', 'NT', 'NU', 'ON', 'PE', 'QC', 'SK', 'YT') NOT NULL DEFAULT 'BC',
+	zip_code VARCHAR(8) NOT NULL
+);
+
+-- create the employee table
+CREATE TABLE employee (
+	emp_id SMALLINT UNSIGNED PRIMARY KEY NOT NULL,
+    works_at TINYINT(2) UNSIGNED NOT NULL,
+    e_type ENUM('Clerk','Manager','SystemAdmin'),
+    FOREIGN KEY (works_at) REFERENCES branch(br_num),
+	FOREIGN KEY (emp_id) REFERENCES users(id_number)
+);
+
+-- create the customer table
+CREATE TABLE customer(
+	cus_id SMALLINT UNSIGNED PRIMARY KEY NOT NULL,
+	standing SET('Good','Probation', 'Suspended') NOT NULL DEFAULT 'Good',
+	cc_Num BIGINT(16),
+	name_on_cCard VARCHAR(20),
+	FOREIGN KEY (cus_id) REFERENCES users(id_number)
+);
+
+-- table for super customers
+CREATE TABLE super_customer(
+	cus_id SMALLINT UNSIGNED PRIMARY KEY NOT NULL,
+	points SMALLINT DEFAULT 500,
+	FOREIGN KEY (cus_id) REFERENCES customer(cus_id)
+);
+
+-- Create Equipment Price table
+CREATE TABLE equipment_price(
+	eq_type ENUM('ski rack', 'child safety seat', 'lift gate', 'car-towing eq') NOT NULL PRIMARY KEY,
+    perWeek DECIMAL(5,2) NOT NULL,
+    perDay DECIMAL(5,2) NOT NULL,
+    perHour DECIMAL(5,2) NOT NULL
+);
+
+-- Create vehicle price table
+CREATE TABLE car_price(
+	class ENUM('economy','compact','midsized','standard','fullsized','premium','luxury','SUV','van') NOT NULL PRIMARY KEY,
+    perHour DECIMAL(5,2) NOT NULL,
+    perDay DECIMAL(5,2) NOT NULL,
+    perWeek DECIMAL(5,2) NOT NULL,
+    perMonth DECIMAL(6,2) NOT NULL,
+    perKM DECIMAL (3,2) NOT NULL
+);
+
+-- Create vehicle price table
+CREATE TABLE truck_price(
+	class ENUM('24-foot','15-foot','12-foot','box-truck') NOT NULL PRIMARY KEY,
+    perHour DECIMAL(5,2) NOT NULL,
+    perDay DECIMAL(5,2) NOT NULL,
+    perWeek DECIMAL(5,2) NOT NULL,
+    perMonth DECIMAL(6,2) NOT NULL,
+    perKM DECIMAL (3,2) NOT NULL
+);
+
+-- Create table insurance_car_price
+CREATE TABLE insurance_car_price(
+	class ENUM('economy','compact','midsized','standard','fullsized','premium','luxury','SUV','van') NOT NULL PRIMARY KEY,
+    perHour DECIMAL(5,2) NOT NULL,
+    perDay DECIMAL(5,2) NOT NULL,
+    perWeek DECIMAL(5,2) NOT NULL,
+    FOREIGN KEY (class) REFERENCES car_price(class)
+);
+
+-- Create table insurance_truck_price
+CREATE TABLE insurance_truck_price(
+	class ENUM('24-foot','15-foot','12-foot','box-truck') NOT NULL PRIMARY KEY,
+    perHour DECIMAL(5,2) NOT NULL,
+    perDay DECIMAL(5,2) NOT NULL,
+    perWeek DECIMAL(5,2) NOT NULL,
+    FOREIGN KEY (class) REFERENCES truck_price(class)
+);
+
+-- Create equipment table
+CREATE TABLE equipment(
+	serial_num SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	location TINYINT(2) UNSIGNED NOT NULL,
+	eq_type ENUM('ski rack', 'child safety seat', 'lift gate', 'car-towing eq') NOT NULL,
+    description VARCHAR(50),
+	FOREIGN KEY (location) REFERENCES branch(br_num),
+	FOREIGN KEY (eq_type) REFERENCES equipment_price(eq_type)
+);
+
+-- Create rental table
+CREATE TABLE rental(
+	reservation_id MEDIUMINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	customer SMALLINT UNSIGNED NOT NULL,
+	start_date DATE NOT NULL,
+	end_date DATE NOT NULL ,
+    start_branch TINYINT(2) UNSIGNED NOT NULL,
+    end_branch TINYINT(2) UNSIGNED NOT NULL,
+    state ENUM('reserved','in-rent','complete'),
+	FOREIGN KEY (customer) REFERENCES customer(cus_id),
+	FOREIGN KEY (start_branch) REFERENCES branch(br_num), 
+    FOREIGN KEY (end_branch) REFERENCES branch(br_num),
+	CONSTRAINT chk_dates CHECK (end_date > start_date)
+);
+
+-- Create rental equipment table
+CREATE TABLE rented_equipment(
+	reservation_id MEDIUMINT UNSIGNED,
+    equipment_id SMALLINT UNSIGNED PRIMARY KEY,
+    FOREIGN KEY (reservation_id) REFERENCES rental(reservation_id),
+    FOREIGN KEY (equipment_id) REFERENCES equipment(serial_num)
+);
+
+-- Create Vehicles table
+CREATE TABLE vehicle(
+	vehicle_id MEDIUMINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	manufacturer VARCHAR(20) NOT NULL,
+	v_year DATE NOT NULL,
+    model VARCHAR(20),
+	color VARCHAR(20),
+	sale_status ENUM('sold', 'for sale', 'for rent') DEFAULT 'for rent'
+);
+
+-- Create car table
+CREATE TABLE car(
+	vehicle_id MEDIUMINT UNSIGNED PRIMARY KEY,
+	class ENUM('economy','compact','midsized','standard','fullsized','premium','luxury','SUV','van') NOT NULL,
+    baggage TINYINT(1) UNSIGNED NOT NULL,
+    door TINYINT(1) UNSIGNED NOT NULL,
+    transmission BOOL DEFAULT FALSE,
+    air_condition BOOL DEFAULT FALSE,
+    FOREIGN KEY (vehicle_id) REFERENCES vehicle(vehicle_id),
+    FOREIGN KEY (class) REFERENCES car_price(class)
+);
+
+-- Create truck table
+CREATE TABLE truck(
+	vehicle_id MEDIUMINT UNSIGNED PRIMARY KEY,
+	class ENUM('24-foot','15-foot','12-foot','box-truck') NOT NULL,
+    seats TINYINT(2) UNSIGNED NOT NULL,
+    clearance DECIMAL(3,1) UNSIGNED NOT NULL,
+    interior_b_l DECIMAL(3,1) UNSIGNED NOT NULL,
+    interior_b_w DECIMAL(3,1) UNSIGNED NOT NULL,
+    interior_b_h DECIMAL(3,1) UNSIGNED NOT NULL,
+    length DECIMAL(3,1) UNSIGNED NOT NULL,
+    width DECIMAL(3,1) UNSIGNED NOT NULL,
+    height DECIMAL(3,1) UNSIGNED NOT NULL,
+    payload_weight_kg DECIMAL(4) UNSIGNED NOT NULL,
+    FOREIGN KEY (vehicle_id) REFERENCES vehicle(vehicle_id),
+    FOREIGN KEY (class) REFERENCES truck_price(class)
+);
+
+-- Create vehicle_sale_price table
+CREATE TABLE vehicle_sale_price(
+	vehicle_id MEDIUMINT UNSIGNED PRIMARY KEY,
+	price DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (vehicle_id) REFERENCES vehicle(vehicle_id)
+);
+
+-- Create branch vehicle table
+CREATE TABLE branch_vehicle(
+	vehicle_id MEDIUMINT UNSIGNED PRIMARY KEY,
+	location TINYINT(2) UNSIGNED NOT NULL,
+    FOREIGN KEY (vehicle_id) REFERENCES vehicle(vehicle_id),
+	FOREIGN KEY (location) REFERENCES branch(br_num)
+);
+
+-- create report
+CREATE TABLE report(
+	report_num MEDIUMINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	report_type ENUM('damage','inspection') NOT NULL,
+	reporting_clerk  SMALLINT UNSIGNED NOT NULL,
+	rental MEDIUMINT UNSIGNED NOT NULL,
+	comments TEXT(500), 
+	FOREIGN KEY (reporting_clerk) REFERENCES employee(emp_id),
+	FOREIGN KEY (rental) REFERENCES rental(reservation_id)
+);
+
+ALTER TABLE vehicle
+ADD path VARCHAR(30);
+
+ALTER TABLE branch
+MODIFY COLUMN street_name VARCHAR(30) NOT NULL;
+
+ALTER TABLE users
+MODIFY COLUMN phone VARCHAR(13);
+
+ALTER TABLE users
+MODIFY COLUMN email VARCHAR(40);
+
+ALTER TABLE account_balance
+MODIFY COLUMN balance DECIMAL(10,2);
+
+ALTER TABLE car
+MODIFY COLUMN door VARCHAR(3);
+ALTER TABLE car
+MODIFY COLUMN transmission BIT DEFAULT 1;
+ALTER TABLE car
+MODIFY COLUMN air_condition BIT DEFAULT 1;
+ALTER TABLE car
+ADD capacity DECIMAL(1);
+
+DROP TABLE truck;
+CREATE TABLE truck(
+	vehicle_id MEDIUMINT UNSIGNED PRIMARY KEY,
+	class ENUM('24-foot','15-foot','12-foot','box-truck') NOT NULL,
+    interior_b_l DECIMAL(4,2) UNSIGNED NOT NULL,
+    interior_b_w DECIMAL(4,2) UNSIGNED NOT NULL,
+    interior_b_h DECIMAL(4,2) UNSIGNED NOT NULL,
+    capacity_kg DECIMAL(5) UNSIGNED NOT NULL,
+    FOREIGN KEY (vehicle_id) REFERENCES vehicle(vehicle_id),
+    FOREIGN KEY (class) REFERENCES truck_price(class)	
+);
+
+ALTER TABLE equipment
+DROP COLUMN description;
