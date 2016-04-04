e8aaeedac3fa2a727813986b98650df9d7e3bc97
samahri <saud.amahri@gmail.com>
2016-04-04 00:12:05 -0700

    Update SuperCustomer.java

--- DIVA/src/accountManagement/SuperCustomer.java
+++ DIVA/src/accountManagement/SuperCustomer.java
@@ -1,19 +1,14 @@
 package accountManagement;
-/**
- * 
- * @author Robin
- *
- */
-public class Employee extends Account{
 
-	private int works_at;
-	private String emp_type;
+public class SuperCustomer extends Customer{
+
+	private int points;
+	//need to implement
 	
-	public Employee(String firstname, String lastname, String phoneNumber, String email, String loginId,
-			String objectClass, int works_at, String emp_type) {
+	public SuperCustomer(String firstname, String lastname, String phoneNumber, String email, String loginId,
+			String objectClass, int points) {
 		super(firstname, lastname, phoneNumber, email, loginId, objectClass);
-		this.works_at = works_at;
-		this.emp_type = emp_type;
+		this.points = points;
 		// TODO Auto-generated constructor stub
 	}
 
