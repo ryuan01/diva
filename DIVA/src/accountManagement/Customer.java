ffe874d058d8df81f7d5ac66b2f6765cb589b81a
samahri <saud.amahri@gmail.com>
2016-04-04 00:10:27 -0700

    Update Customer.java

--- DIVA/src/accountManagement/Customer.java
+++ DIVA/src/accountManagement/Customer.java
@@ -1,5 +1,18 @@
 package accountManagement;
 
-public abstract class Customer  {
-
+public class Customer extends Account{
+	private String standing;
+	private long cc_num;
+	private String name_on_card;
+	private String street_name;
+	private String city;
+	private String province;
+	private String zip;
+	
+	//need to implement
+	public Customer(String firstname, String lastname, String phoneNumber, String email, String loginId,
+			String objectClass) {
+		super(firstname, lastname, phoneNumber, email, loginId, objectClass);
+		// TODO Auto-generated constructor stub
+	}
 }
