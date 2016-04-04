9c90343675e471b14759881b853ec2a3f4e3799d
samahri <saud.amahri@gmail.com>
2016-04-04 00:10:54 -0700

    Update SuperCustomer.java

--- DIVA/src/accountManagement/SuperCustomer.java
+++ DIVA/src/accountManagement/SuperCustomer.java
@@ -1,5 +1,20 @@
 package accountManagement;
+/**
+ * 
+ * @author Robin
+ *
+ */
+public class Employee extends Account{
 
-public class SuperCustomer {
+	private int works_at;
+	private String emp_type;
+	
+	public Employee(String firstname, String lastname, String phoneNumber, String email, String loginId,
+			String objectClass, int works_at, String emp_type) {
+		super(firstname, lastname, phoneNumber, email, loginId, objectClass);
+		this.works_at = works_at;
+		this.emp_type = emp_type;
+		// TODO Auto-generated constructor stub
+	}
 
 }
