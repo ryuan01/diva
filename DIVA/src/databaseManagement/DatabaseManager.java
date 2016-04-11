40f370e37d0798e33130b28a723a1df064a7d482
Zhengxiong Zhang <BenZhang@Zhengxiongs-MacBook-Air.local>
2016-04-11 16:59:53 -0700

    update paymentMan, dbMan, Receipt

--- DIVA/src/databaseManagement/DatabaseManager.java
+++ DIVA/src/databaseManagement/DatabaseManager.java
@@ -1,33 +1,96 @@
+/**
+ * Class DatabaseManager is fully implemented and tested
+ */
+
 package databaseManagement;
 
-/*Robin */
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.SQLException;
+import accountManagement.Account;
+import rentalManagement.Report;
+import rentalManagement.Reservation;
+import systemManagement.Branch;
+import vehicleManagement.Car;
+import vehicleManagement.Truck;
+import vehicleManagement.Vehicle;
+
 /**
- * DatabaseManager deals with connection to database
- * @author Robin
- * @invariant dbname database name 
- * @invariant pw password for connection 
+ * DatabaseManager deals with all functions that interacts with database
+ * @author Robin, Sammy
  */
 public class DatabaseManager {
 	
-	protected String dbname;
-	protected String pw;
+	// I am modeling as Has-A relationship
+	// Not sure if I need to change this? 
+	private ConnectDB conDB; // you don't need to create a ConnectDB object here
+	//private AccountDB accDB;
+	private BranchDB branDB;
+	//private EquipmentDB eqDB;
+	private RentalDB reDB;
+	private VehicleDB veDB;
+	private PriceDB prDB;
+    
+    //singieton design pattern
+    private static DatabaseManager instance = null;
+ 
+    /**
+     * Get the only databaseManager
+     * @return an instance of databasemanager
+     * @post databaseManager.instance != null
+     */
+    public static DatabaseManager getInstance(){
+    	if (instance == null){
+    		instance = new DatabaseManager();
+    	}
+    	return instance;
+    }
+    
+    //called by shutDown method in system manager
+    /**
+     * Destroys instance
+     * @pre instance = new DatabaseManager()
+     * @post instance = null
+     */
+    public static void destroyDatabase()
+    {
+    	instance = null;
+    }
+    
+	/** 
+	* Constructs a DatabaseManager
+	* @post an only DatabasManager object is created
+	*/
+    private DatabaseManager(){
+		conDB = new ConnectDB();
+		//accDB = new AccountDB();
+		branDB = new BranchDB();
+		//eqDB = new EquipmentDB();
+		reDB = new RentalDB();
+		veDB = new VehicleDB();
+		prDB = new PriceDB();
+    }
+       
 	
-	/**
-	 * Constructs a DatabaseManager
-	 * @param db a db name
-	 * @param pass an encrypted password
-	 * @pre db and pass are valid
-	 * @post a DatabasManager object is created
-	 */
-	public DatabaseManager(String db, String pass) {
-	}
-	
+    // ConnectDB
 	/**
 	 * Connect to database
 	 * @pre !isConnect()
 	 * @post isConnected() 
 	 */
 	public void connect() {
+		// you don't need to create a ConnectDB object here
+		conDB.connect();
+	}
+	
+	/**
+	 * Get the connection
+	 * @pre none
+	 * @post none
+	 * @return a Connection object
+	 */
+	public Connection getConnection(){
+		return conDB.getConnection();
 	}
 	
 	/**
@@ -36,15 +99,338 @@ public class DatabaseManager {
 	 * @post !isConnect()
 	 */
 	public void disconnect() {
+		conDB.disconnect();
+	}
+ 
+	//Branch Related
+	/**
+	 * 
+	 * @param b
+	 * @throws SQLException
+	 */
+	public void createBranchEntry(Branch b) throws SQLException
+	{
+		branDB.addBranch(b);
+	}
+	
+	/*
+	 * not relevant
+	public boolean modifyBranchEntry(int id,String address, String city, String province, String zipcode)
+	{
+		return true;
+	}*/
+	
+	/**
+	 * 
+	 * @param id
+	 * @throws SQLException
+	 */
+	public void removeBranchEntry(int id) throws SQLException
+	{
+		branDB.removebranch(id);
 	}
 	
 	/**
-	 * Checks if there is a connection
-	 * @pre none
-	 * @post returns true if there is a connection, otherwise false 
+	 * 
+	 * @param id
+	 * @return
+	 * @throws SQLException
 	 */
-	public boolean isConnected() {
-		return false;
+	public Branch getBranchEntry(int id) throws SQLException
+	{
+		return branDB.getBranch(id);
+	}
+	
+
+	// EquipmentDB
+
+	// probably should be able to add/delete equipments, modify them
+
+   
+	// RentalDB
+	
+	/**
+	 * Create an reservation entry in database
+	 * @param r reservation
+	 * @throws SQLException 
+	 * @pre r.id is null
+	 * @pre r is not in database
+	 * @post r.id is updated 
+	 */
+	public void createReservationEntry(Reservation r) throws SQLException {
+		// Look at the note in getBranch method
+		reDB.createReservation(r);
+	}
+	
+	// can be implemented, removes a Reservation from the database completely, not archived.
+	public void removeReservationEntry(int reservID) {
+		// TODO Auto-generated method stub
 	}
 
+	// can be implemented, changes status of reservation only.
+	public void changeReservationStatus(int reservID, String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	// updated signature
+	public void modifyReservationEntries(int reservID, String startDate,String endDate, int vehicleID, int[] equipIDs, int startBranchID, int endBranchID, 
+			int customerID, int employeeID, String status) {
+		// TODO Auto-generated method stub
+	}
+	
+	public Reservation searchReservationEntry(int reservID) throws SQLException {
+		// TODO Auto-generated method stub
+		return reDB.reservationQuery(reservID);
+	}
+
+
+	public void addReport(Report r)
+	{
+	}
+	
+	public String getReservationEndDate(int reservID)
+	{
+		return null;
+	}
+	
+	
+	public Account getReservationAccount(int reservID)
+	{
+		return null;
+	}
+	
+	// returns truck or car.
+	public Vehicle getReservationVehicle(int reservID)
+	{
+		return null;
+	}
+	
+	//VehicleDB
+	/**
+	 * Generic search of vehicle available at certain date, certain branch 
+	 * @param c
+	 * @param branch_id
+	 * @param type
+	 * @param time
+	 * @param end_date 
+	 * @param list
+	 * @return
+	 * @throws SQLException 
+	 */
+	public Vehicle[] search(int branch_id, String type, String start_date, String end_date) throws SQLException{
+		//System.out.println("Connected, trying to insert next");
+		Vehicle[] vlist = veDB.search(branch_id,type,start_date, end_date);
+		return vlist;
+	}
+	
+	/**
+	 * 
+	 * @param branch_id
+	 * @param type
+	 * @return
+	 * @throws SQLException
+	 */
+	public Vehicle[] search(int branch_id, String type) throws SQLException {
+		// TODO Auto-generated method stub
+		Vehicle[] vlist = null;
+		if (type.equals("car")){
+			vlist = veDB.searchOverdueCars(branch_id);
+		}
+		else if (type.equals("truck")){
+			vlist = veDB.searchOverdueTrucks(branch_id);
+		}
+		else{
+			throw new IllegalArgumentException("Type must be 'car' or 'truck'");
+		}
+		return vlist;
+	}
+	
+	/**
+	 * 
+	 * @param branch_id
+	 * @param type
+	 * @return
+	 * @throws SQLException
+	 */
+	public Vehicle[] searchForSale(int branch_id, String type) throws SQLException {
+		// TODO Auto-generated method stub
+		Vehicle[] vlist = null;
+		if (type.equals("car")){
+			vlist = veDB.searchForsaleCars(branch_id);
+		}
+		else if (type.equals("truck")){
+			vlist = veDB.searchForsaleTrucks(branch_id);
+		}
+		else{
+			throw new IllegalArgumentException("Type must be 'car' or 'truck'");
+		}
+		return vlist;
+	}
+	
+	
+	// Account related
+	
+	public void createAccountEntry(Account a)
+	{
+	}
+	
+	public void removeAccountEntry(String userName)
+	{
+	}
+	
+	public Account[] searchAccountEntries(String firstName, String lastName, String phoneNumber, String emailAddress, String userName, String status)
+	{
+		return null;
+	}
+	
+	public String retrievePassword(String userName)
+	{
+		return null;
+	}
+	
+	// find account by loginID, loginID should be immutable
+	public void modifyAccountEntry(String firstname, String lastname, String phoneNumber, String email, String loginId, String status)
+	{
+	}
+	
+	public void changeAccountStatus(String userName, String status)
+	{
+	}
+	
+	public void addSRPoints(String userName, int points)
+	{
+	}
+	public void addSRPoints(int i, int points) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void modifyPassword(String userName, String newPassword)
+	{
+	}
+
+	/**
+	 * Update vehicle's owning branch
+	 * @pre vehicle is always assumed to be at its owning branch
+	 * @param v vehicle_id
+	 * @param b branch_id
+	 * @throws SQLException when the update fails
+	 */
+	public void updateVehicleLocation(int v, int b) throws SQLException {
+		// TODO Auto-generated method stub
+		veDB.updateVehicleLocation(v, b);
+	}
+
+	/**
+	 * Update vehicle's status from {for rent, for sale, sold}
+	 * @param v vehicle_id
+	 * @param status from {for rent, for sale, sold}
+	 * @throws SQLException when the update fails
+	 */
+	public void updateVehicleStatus(int v, String status) throws SQLException {
+		// TODO Auto-generated method stub
+		veDB.updateVehicleStatus(v, status);
+	}
+
+
+	/**
+	 * Add a vehicle to database
+	 * @param v {car, truck}
+	 * @throws IllegalArgumentException
+	 * @throws SQLException
+	 */
+	public void addVehicle(Vehicle v) throws IllegalArgumentException, SQLException{
+		// TODO Auto-generated method stub
+		if (v instanceof Car){
+			veDB.addCar((Car) v);
+		}
+		else if (v instanceof Truck){
+			veDB.addTruck((Truck)v);
+		}
+		else { //none of above
+			throw new IllegalArgumentException("addVehicle only takes vehicle of type Car or Truck");
+		}
+	}
+	
+	// USED BY PAYMENT MANAGER
+	
+	// returns BigDecimal[9][5]
+	public BigDecimal[][] getCarPriceList() throws SQLException
+	{
+		return prDB.getCarPriceList();
+	}
+
+	// returns BigDecimal[4][5]
+	public BigDecimal[][] getTruckPriceList() throws SQLException
+	{
+		return prDB.getTruckPriceList();
+	}
+
+	// returns BigDecimal[4][3]
+	public BigDecimal[][] getEquipmentPriceList() throws SQLException
+	{
+		return prDB.getEquipmentPriceList();
+	}
+	
+	// returns BigDecimal[9][3]
+	public BigDecimal[][] getCarInsurancePriceList() throws SQLException
+	{
+		return prDB.getCarInsurancePriceList();
+	}
+	
+	// returns BigDecimal[3][9]
+	public BigDecimal[][] getTruckInsurancePriceList() throws SQLException
+	{
+		return prDB.getTruckInsurancePriceList();
+	}
+	
+	// sets BigDecimal[5][9]
+	public boolean setCarPriceList(BigDecimal[][] a)
+	{
+		return true;
+	}
+
+	// sets BigDecimal[5][4]
+	public boolean setTruckPriceList(BigDecimal[][] a)
+	{
+		return true;
+	}
+
+	// sets BigDecimal[3][4]
+	public boolean setEquipmentPriceList(BigDecimal[][] a)
+	{
+		return true;
+	}
+	
+	// sets BigDecimal[3][9]
+	public boolean setCarInsurancePriceList(BigDecimal[][] a)
+	{
+		return true;
+	}
+	
+	// sets BigDecimal[3][9]
+	public boolean setTruckInsurancePriceList(BigDecimal[][] a)
+	{
+		return true;
+	}
+
+	/**
+	 * Remove vehicle from database 
+	 * @param vehicle_id id that uniquely identifies vehicle
+	 * @param type type of vehicle {'car','truck'}
+	 * @throws SQLException 
+	 */
+	public void removeVehicle(int vehicle_id, String type) throws SQLException {
+		// TODO Auto-generated method stub
+		if (type.equals("car")){
+			veDB.removeCar(vehicle_id);
+		}
+		else if (type.equals("truck")){
+			veDB.removeTruck(vehicle_id);
+		}
+		else {
+			throw new IllegalArgumentException("Vehicle can only be of 'car' or 'truck");
+		}
+	}
 }
--- DIVA/src/paymentManager/Receipt.java
+++ DIVA/src/paymentManager/Receipt.java
@@ -1,16 +1,23 @@
-package paymentManager;
+package paymentManagement;
+
+import java.math.BigDecimal;
+
+import webServiceManagement.ArrayOfStringsable;
 
 /**
  * 
  * A receipt that is provided to the customer after a rental has been made.
  *
  */
-public class Receipt {
+public class Receipt implements ArrayOfStringsable{
 
-	private double price;
+	private int receiptId;
+	private BigDecimal price;
 	private String vehicle_rented;
-	private String duration;
-	private String dropoff_location;
+	private String startDate;
+	private String endDate;
+	private String pickup_branch;
+	private String dropoff_branch;
 	
 	
 	/**
@@ -20,11 +27,14 @@ public class Receipt {
 	 * @param duration
 	 * @param dropoff_location
 	 */
-	public Receipt(double price, String vehicle_rented, String duration, String dropoff_location) {
+	public Receipt(int receiptId ,BigDecimal price, String vehicle_rented, String startDate, String endDate, String pickup_branch,String dropoff_branch) {
+		this.receiptId = receiptId;
 		this.price = price;
 		this.vehicle_rented = vehicle_rented;
-		this.duration = duration;
-		this.dropoff_location = dropoff_location;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.pickup_branch = pickup_branch;
+		this.dropoff_branch = dropoff_branch;
 	}
 
 	
@@ -32,14 +42,14 @@ public class Receipt {
 	 *  Gets a Price of the Rental.
 	 * @return price
 	 */
-	public double getPrice() {
+	public BigDecimal getPrice() {
 		return price;
 	}
 	/**
 	 * Sets the Price of the rental.
 	 * @param price
 	 */
-	public void setPrice(double price) {
+	public void setPrice(BigDecimal price) {
 		this.price = price;
 	}
 	/**
@@ -56,35 +66,27 @@ public class Receipt {
 	public void setVehicle_rented(String vehicle_rented) {
 		this.vehicle_rented = vehicle_rented;
 	}
+
 	/**
-	 * Gets the duration of the Rental.
-	 * @return duration
+	 * Gets the Receipt Id of the Receipt.
+	 * @return receiptId
 	 */
-	public String getDuration() {
-		return duration;
+	public int getReceiptId() {
+		return receiptId;
 	}
+
 	/**
-	 * Sets the duration of the rental.
-	 * @param duration
+	 * Sets the Receipt Id of the Receipt
+	 * @param receiptId
 	 */
-	public void setDuration(String duration) {
-		this.duration = duration;
-	}
-	/**
-	 * Get the Drop off Location After Rental has been completed.
-	 * @return dropoff_location
-	 */
-	public String getDropoff_location() {
-		return dropoff_location;
-	}
-	/**
-	 * Set the Location to be Dropped off when Rental is completed.
-	 * @param dropoff_location
-	 */
-	public void setDropoff_location(String dropoff_location) {
-		this.dropoff_location = dropoff_location;
+	public void setReceiptId(int receiptId) {
+		this.receiptId = receiptId;
 	}
 	
-	
+	public String toString() {
+		return "{'receiptId':'"+ this.receiptId +"', 'price':'"+this.price+"',"
+			+	"'vehicle_rented':'"+this.vehicle_rented+"',"+"'start_Date':'"+this.startDate+"','end_Date':'"+this.endDate+ "',pickUp_branch':'" + this.pickup_branch
+			+"',dropOff_branch':'" + this.dropoff_branch + "'}";
+	}	
 	
 }
