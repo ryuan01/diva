19a2c93dc491998c434a59cda8e8b70105f3705c
Sammy <saud.amahri@gmail.com>
2016-04-12 18:09:20 -0700

    got Report.java

--- DIVA/src/rentalManagement/Report.java
+++ DIVA/src/rentalManagement/Report.java
@@ -12,9 +12,10 @@ public class Report implements ArrayOfStringsable{
 	private static final int MAX_MILEGE = 0; 
 	private String objectClass;
 	private int report_num;
+	private int report_clerk_id;
 	private String reportDate;
 	private String reportDescription;
-	private int reportReservationID;
+	private int reportRentalID;
 	private int milage; //0 - MAX_MILEGE
 	private int gasLevel; //unsigned int between 0-100
 	
@@ -25,17 +26,19 @@ public class Report implements ArrayOfStringsable{
 	 *  A Report with date, description, and associated reservation.
 	 * @param d
 	 * @param description
-	 * @param reservID
+	 * @param rentalID
 	 * @param milage
 	 * @param gasLevel
 	 */
-	public Report(String d, String description, int reservID, int milage, int gasLevel)
+	public Report(int clerk_id, String d, String description, int rentalID, int milage, int gasLevel, int report_num)
 	{
+		this.report_num = report_num;
 		objectClass = getClass().getName();
 		report_num = -1; //this need to be updated from database 
+		report_clerk_id = clerk_id;
 		reportDate = d;
 		reportDescription = description;
-		reportReservationID = reservID;
+		reportRentalID = rentalID;
 		this.milage = milage;
 		this.gasLevel = gasLevel;
 	}
@@ -64,7 +67,23 @@ public class Report implements ArrayOfStringsable{
 	 */
 	public void changeReportReservationID(int newReportReservationID)
 	{
-		reportReservationID = newReportReservationID;
+		reportRentalID = newReportReservationID;
+	}
+	
+	/**
+	 * Modifies the reporting clerk id
+	 * @param clerk_id identifies clerk
+	 */
+	public void changeReportClerk(int clerk_id){
+		report_clerk_id = clerk_id;
+	}
+	
+	/**
+	 * Returns reporting clerk ID
+	 * @return ID that identifies which clerk is in charge of this report. 
+	 */
+	public int getReportClerk(){
+		return report_clerk_id;
 	}
 	
 	/**
@@ -91,7 +110,7 @@ public class Report implements ArrayOfStringsable{
 	 */
 	public int getReportReservationID()
 	{
-		return reportReservationID;
+		return reportRentalID;
 	}
 	
 	/**
@@ -109,5 +128,21 @@ public class Report implements ArrayOfStringsable{
 
 	public void setObjectClass(String objectClass) {
 		this.objectClass = objectClass;
+	}
+	
+	public void setGasLevel(int g){
+		gasLevel = g;
+	}
+	
+	public void setMilage(int m){
+		milage = m;
+	}
+	
+	public int getGasLevel(){
+		return gasLevel;
+	}
+	
+	public int getMilage(){
+		return milage;
 	}
 }
