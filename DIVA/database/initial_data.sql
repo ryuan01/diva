5a62dba4f87e993ac5c9feeb5e62d722dec17895
Sammy <saud.amahri@gmail.com>
2016-04-17 14:51:19 -0700

    Create initial_data.sql

---
+++ DIVA/database/initial_data.sql
@@ -0,0 +1,7 @@
+-- Branch
+INSERT INTO `branch` VALUES(1, '100 Manitoba Street', 'Vancouver', 'BC', 'V5Y2Z6');
+INSERT INTO `branch` VALUES(2, '3211 Grant McConachie Way', 'Richmond', 'BC', 'V7B 0A');
+INSERT INTO `branch` VALUES(3, '757 Hornby Street', 'Vancouver', 'BC', 'V6Z1S2');
+INSERT INTO `branch` VALUES(4, ' 100 Manitoba Street', 'Vancouver', 'BC', 'V4TZ7T');
+INSERT INTO `branch` VALUES(5, '2871 East 1st', 'Vancouver', 'BC', 'V7S1A1');
+INSERT INTO `branch` VALUES(8, '123 Fake Street', 'Saskatoon', 'SK', 'V8R6S1');
